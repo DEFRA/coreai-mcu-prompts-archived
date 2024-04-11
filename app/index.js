2080418c243020ddd4d421b70b4a2b493dc53b3c
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2024-04-11 17:00:26 +0100

    Add router

--- app/index.js
+++ app/index.js
@@ -1,13 +1,10 @@
-const server = require('./server')
+require('./insights').setup()
+const createServer = require('./server')
 
 const init = async () => {
+  const server = await createServer()
   await server.start()
   console.log('Server running on %s', server.info.uri)
 }
-
-process.on('unhandledRejection', (err) => {
-  console.log(err)
-  process.exit(1)
-})
 
 init()
